47983d7a66d2870a51beccb632139e69e61ec794
TheoWattman <159834319+TheoWattman@users.noreply.github.com>
2025-04-03 09:00:40 +0200

    Add files via upload

--- extension-web/server.js
+++ extension-web/server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
+
 
 const app = express();
-
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
