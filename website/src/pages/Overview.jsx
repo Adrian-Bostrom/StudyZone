a80a7ac49f3577e9babdd38043d1227bf8dc0464
Marcus Söderlund <marcus.soderlund04@gmail.com>
2025-05-07 13:53:24 +0200

    small fixes

--- website/src/pages/Overview.jsx
+++ website/src/pages/Overview.jsx
@@ -1,13 +1,25 @@
-import CourseCard from "./components/CourseCard";
+import React from "react";
+import DeadlineBox from "./components/DeadlineBox";
 
 const Overview = () => {
   return (
 
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-        <CourseCard course="SF1654" />
-        <h1 className="text-4xl font-bold mb-4">Overview</h1>
-        <p className="text-gray-600 text-center">This is the overview page.</p>
-    </div>
+    <div className="border-1 border-e-red-500 flex flex-row min-h-screen">
+        {/* Course Column */}
+        <div className="border-1 flex flex-col bg-gray-100 p-12 flex-grow">
+          
+          <h1 className="border-1 text-5xl font-bold mb-1">
+            Overview</h1>
+          <p className="border-1 text-gray-600 text-start max-w-2xl">
+            Here is an overview of all your courses and their assignments
+            </p>
+          
+        </div>
+        {/* Deadline box */}
+        <div className="border-1 right-0 top-0 w-100 flex-col items-start">
+          <DeadlineBox/>
+        </div>
+      </div>
   );
 }
 
--- website/src/pages/components/Navbar.jsx
+++ website/src/pages/components/Navbar.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const userID = localStorage.getItem("userID"); // Retrieve userID from local storage
+
     return (
         <nav className="bg-gray-800 text-white p-4 h-20 flex items-center shadow-md font-thin">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-xl font-bold">
-                    <Link to="/">StudyZone</Link>
+                    <Link to={userID ? "/" : "/overview"}>StudyZone</Link>
                 </div>
                 <ul className="flex space-x-4">
                     <li className="hover:text-gray-300">
@@ -16,7 +18,7 @@ const Navbar = () => {
                         <Link to="/login">Login</Link>
                     </li>
                     <li className="hover:text-gray-300">
-                        <Link to="/Courses">Courses</Link>
+                        <Link to="/overview">Overview</Link>
                     </li>
                     <li className="hover:text-gray-300">
                         <Link to="/Schedule">Schedule</Link>
