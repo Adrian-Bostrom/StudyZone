abf33b2f52e49dbb116dc0d6d2ee2780a478fbeb
William Sjöberg <William.sjoberg12@gmail.com>
2025-05-07 10:52:57 +0200

    fetch json för api calls

--- website/src/pages/components/UseFetchJson.jsx
+++ website/src/pages/components/UseFetchJson.jsx
@@ -1,20 +1,32 @@
 import { useState, useEffect } from "react";
 
-const UseFetchJson = (url) => {
-  const [data, setData] = useState([]);
+const UseFetchJson = (url, bodyData) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch(url, {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bodyData),
     })
-      .then(response => response.json())
-      .then(myJson => setData(myJson));
-  }, [url]);
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((jsonData) => {
+        setData(jsonData);
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  }, [url, bodyData]);
 
-  return data;
-}
+  return { data, error };
+};
 
 export default UseFetchJson;
